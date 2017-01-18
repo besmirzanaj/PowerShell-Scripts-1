bee29620fadc549b6a7eb9fdc7e4ce57663e444b
omniomi <omniomi@gmail.com>
2017-01-18 10:15:40 -0600

    Function to Convert-FrenchCharacters to basic Latin characters.
    Function to Convert-FrenchCharacters to basic Latin characters.

---
+++ Convert-FrenchCharacters.ps1
@@ -0,0 +1,43 @@
+﻿<#
+	.SYNOPSIS
+		Remove French special characters.
+	
+	.DESCRIPTION
+		Convert a string/name to basic Latin characters.
+	
+	.PARAMETER String
+		String or Name to be converted.
+	
+	.EXAMPLE
+		PS C:\> Convert-FrenchCharacters -String 'François Gérard'
+
+		Francois Gerard
+
+#>
+function Convert-FrenchCharacters
+{
+	[CmdletBinding()]
+	[OutputType([string])]
+	param
+	(
+		[Parameter(Mandatory = $true,
+				   Position = 0)]
+		[ValidateNotNullOrEmpty()]
+		[Alias('Name')]
+		[string]
+		$String
+	)
+	
+	$Output = New-Object System.Text.StringBuilder
+	
+	$NoSpecial = $String.Normalize("FormD")
+	
+	$NoSpecial.ToCharArray() | ForEach-Object -Process {
+		if ([Globalization.CharUnicodeInfo]::GetUnicodeCategory($psitem) -ne [Globalization.UnicodeCategory]::NonSpacingMark)
+		{
+			[void]$Output.Append($psitem)
+		}
+	}
+	
+	Return $Output.ToString()
+}
